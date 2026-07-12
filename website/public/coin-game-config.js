1801501b5d070576edcf404cadc0a76a2bb33187
MATT | ADL | BLOCK #0080 <mattyoungaviation@gmail.com>
2026-07-12 12:20:07 -0700

    Add MATT coin flip website configuration

---
+++ website/public/coin-game-config.js
@@ -0,0 +1,9 @@
+window.MATT_COIN_FLIP_CONFIG = Object.freeze({
+  chainId: 2020,
+  tokenAddress: "0xa5450417BDCa0BDfB058ffE41205400FfDA1174d",
+  treasuryAddress: "0xF79913cB83Cc9CABD95D0ba9250103fbb939f984",
+  contractAddress: "",
+  explorerAddressBase: "https://explorer.roninchain.com/address/",
+  minimumBetMatt: "1",
+  maximumBetMatt: "1000000"
+});
