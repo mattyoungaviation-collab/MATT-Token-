b87b74e43c43e73b516cc4c361035e46c1ff2956
MATT | ADL | BLOCK #0080 <mattyoungaviation@gmail.com>
2026-07-13 16:40:46 -0700

    Add verified rewards V2 website configuration

---
+++ website/public/daily-rewards-v2-config.js
@@ -0,0 +1,9 @@
+window.MATT_DAILY_REWARDS_V2_CONFIG = Object.freeze({
+  chainId: 2020,
+  contractAddress: "0x0000000000000000000000000000000000000000",
+  deploymentBlock: 0,
+  rewardAmountMatt: "1000000",
+  cooldownSeconds: 86400,
+  xHandle: "crafting_skill",
+  explorerTransactionBase: "https://explorer.roninchain.com/tx/"
+});
