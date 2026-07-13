d11163068f4abcd51e2f4d3c7dfff7ae78b50fe6
MATT | ADL | BLOCK #0080 <mattyoungaviation@gmail.com>
2026-07-13 07:34:05 -0700

    Add daily rewards website configuration

---
+++ website/public/daily-rewards-config.js
@@ -0,0 +1,11 @@
+window.MATT_DAILY_REWARDS_CONFIG = Object.freeze({
+  chainId: 2020,
+  contractAddress: "0x0000000000000000000000000000000000000000",
+  deploymentBlock: 0,
+  rewardAmountMatt: "2000000",
+  cooldownSeconds: 86400,
+  xHandle: "crafting_skill",
+  xUrl: "https://x.com/crafting_skill",
+  explorerAddressBase: "https://explorer.roninchain.com/address/",
+  explorerTransactionBase: "https://explorer.roninchain.com/tx/"
+});
