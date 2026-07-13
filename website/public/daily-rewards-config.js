3450fe53906a89a384604ba673f7ff9c65eb269c
MATT | ADL | BLOCK #0080 <mattyoungaviation@gmail.com>
2026-07-13 07:59:14 -0700

    Activate live daily rewards contract

--- website/public/daily-rewards-config.js
+++ website/public/daily-rewards-config.js
@@ -1,7 +1,7 @@
 window.MATT_DAILY_REWARDS_CONFIG = Object.freeze({
   chainId: 2020,
-  contractAddress: "0x0000000000000000000000000000000000000000",
-  deploymentBlock: 0,
+  contractAddress: "0x30B581960493f4FCEa36D8EA7E970CAE1835b742",
+  deploymentBlock: 58242948,
   rewardAmountMatt: "2000000",
   cooldownSeconds: 86400,
   xHandle: "crafting_skill",
