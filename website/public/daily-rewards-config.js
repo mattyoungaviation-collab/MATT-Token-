af3dedb01c54e8089788e0889b35a3c4d18fd043
MATT | ADL | BLOCK #0080 <mattyoungaviation@gmail.com>
2026-07-13 16:39:34 -0700

    Disable legacy self-attested rewards

--- website/public/daily-rewards-config.js
+++ website/public/daily-rewards-config.js
@@ -1,11 +1,12 @@
 window.MATT_DAILY_REWARDS_CONFIG = Object.freeze({
   chainId: 2020,
-  contractAddress: "0x30B581960493f4FCEa36D8EA7E970CAE1835b742",
-  deploymentBlock: 58242948,
-  rewardAmountMatt: "2000000",
+  contractAddress: "0x0000000000000000000000000000000000000000",
+  deploymentBlock: 0,
+  rewardAmountMatt: "1000000",
   cooldownSeconds: 86400,
   xHandle: "crafting_skill",
   xUrl: "https://x.com/crafting_skill",
   explorerAddressBase: "https://explorer.roninchain.com/address/",
-  explorerTransactionBase: "https://explorer.roninchain.com/tx/"
+  explorerTransactionBase: "https://explorer.roninchain.com/tx/",
+  migrationNotice: "Daily rewards are temporarily paused while verified X OAuth rewards V2 is deployed."
 });
