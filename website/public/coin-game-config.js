d33e0f858b8291d12e86e8525ae829313fceb2f3
MATT | ADL | BLOCK #0080 <mattyoungaviation@gmail.com>
2026-07-14 15:39:36 -0700

    Point MATT Hub to live BurnFlip contract

--- website/public/coin-game-config.js
+++ website/public/coin-game-config.js
@@ -1,9 +1,10 @@
 window.MATT_COIN_FLIP_CONFIG = Object.freeze({
   chainId: 2020,
   tokenAddress: "0xa5450417BDCa0BDfB058ffE41205400FfDA1174d",
-  treasuryAddress: "0xF79913cB83Cc9CABD95D0ba9250103fbb939f984",
-  contractAddress: "0x4C014Eb6F1e65c97A006beC8c8F2fB8Fbbf5F5aB",
+  contractAddress: "0xd6B6E08fB04b2Ee76e6584f49c6adE75d7ad144e",
+  deploymentBlock: 58299639,
   explorerAddressBase: "https://explorer.roninchain.com/address/",
   minimumBetMatt: "1",
-  maximumBetMatt: "1000000"
+  maximumBetMatt: "10000000000",
+  burnEdition: true
 });
