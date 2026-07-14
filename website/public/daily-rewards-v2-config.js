9668afc8dff8962e20d079b80776fd2e93c92735
MATT | ADL | BLOCK #0080 <mattyoungaviation@gmail.com>
2026-07-13 17:55:15 -0700

    Activate verified 1M MATT daily rewards V2

--- website/public/daily-rewards-v2-config.js
+++ website/public/daily-rewards-v2-config.js
@@ -1,7 +1,7 @@
 window.MATT_DAILY_REWARDS_V2_CONFIG = Object.freeze({
   chainId: 2020,
-  contractAddress: "0x0000000000000000000000000000000000000000",
-  deploymentBlock: 0,
+  contractAddress: "0xd45Cfe08F161e8D16CA052fE6ad721506A35018c",
+  deploymentBlock: 58260816,
   rewardAmountMatt: "1000000",
   cooldownSeconds: 86400,
   xHandle: "crafting_skill",
