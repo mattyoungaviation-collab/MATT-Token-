7aeb46a8f1ca1c3dd157fa83295599270ce115ab
mattyoungaviation-collab <mattyoungaviation@gmail.com>
2026-08-05 09:09:17 -0700

    Publish verified paused Slots deployment

--- website/public/slots-config.js
+++ website/public/slots-config.js
@@ -1,23 +1,23 @@
 (() => {
   "use strict";
   window.MATT_SLOTS_CONFIG = Object.freeze({
-    chainId: 2020,
-    chainHex: "0x7e4",
-    chainName: "Ronin Mainnet",
-    rpcUrl: "/api/rpc",
-    tokenAddress: "0xa5450417BDCa0BDfB058ffE41205400FfDA1174d",
-    tokenSymbol: "MATT",
-    treasuryAddress: "0xBacE355D23d378a6E1adD986E53a18Dd12E6EeAc",
-    vrfCoordinatorAddress: "0x16a62a921e7fec5bf867ff5c805b662db757b778",
-    slotsAddress: null,
-    rewardVaultAddress: null,
-    converterAddress: null,
-    deploymentBlock: null,
-    declaredRtp: 97,
-    maxMultiplier: 500,
-    defaultBet: "5000",
-    defaultQuantity: 10,
-    explorerBase: "https://app.roninchain.com",
-    practiceEnabled: true
-  });
+    "chainId": 2020,
+    "chainHex": "0x7e4",
+    "chainName": "Ronin Mainnet",
+    "rpcUrl": "/api/rpc",
+    "tokenAddress": "0xa5450417BDCa0BDfB058ffE41205400FfDA1174d",
+    "tokenSymbol": "MATT",
+    "treasuryAddress": "0xBacE355D23d378a6E1adD986E53a18Dd12E6EeAc",
+    "vrfCoordinatorAddress": "0x16A62a921e7fEC5Bf867fF5c805b662Db757B778",
+    "slotsAddress": "0xa4d1bE4bb63204D924D81E927E2ba95ce83bf589",
+    "rewardVaultAddress": "0x0c9C78880D48d0ce93E52713DDDB7F25996D234A",
+    "converterAddress": "0xCe4CBBD4d2Ee93a678297E3a27cb14Ece80A04a2",
+    "deploymentBlock": 59237804,
+    "declaredRtp": 97,
+    "maxMultiplier": 500,
+    "defaultBet": "5000",
+    "defaultQuantity": 10,
+    "explorerBase": "https://app.roninchain.com",
+    "practiceEnabled": true
+});
 })();
